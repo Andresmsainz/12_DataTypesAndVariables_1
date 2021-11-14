761b1cf7b45c6cb7fbbba33e346a5bdf2d475366
Andres Sainz <andresmsainz@gmail.com>
2021-11-14 14:09:21 -0500

    initial commit

---
+++ 12_DataTypesAndVariables_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_DataTypesAndVariables_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            Console.WriteLine("Number x is " + x);
+            //Change on the fly
+            x = 5;
+            Console.WriteLine("Number x is " + x);
+
+            double dbl_num = 0.0d;
+            float flt_num = 0.0f;
+            decimal dcml_num = 0.0m;
+
+            dbl_num = 1.1d;
+            flt_num = 2.2f;
+            dcml_num = 3.3m;
+
+            Console.WriteLine("Double variable = " + dbl_num);
+            Console.WriteLine("Float variable = " + flt_num);
+            Console.WriteLine("Decimal variable = " + dcml_num);
+        }
+    }
+}
